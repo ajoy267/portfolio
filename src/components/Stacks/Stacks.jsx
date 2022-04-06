ad094638458e4986ac471b6090b2c25aea411e94
ajoy267 <andrew.joy12@hotmail.com>
2022-04-06 15:41:00 -0700

    made the file for my tech stack section

---
+++ src/components/Stacks/Stacks.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Stacks() {
+  return (
+    <section>
+      <h4>Tech Stack</h4>
+    </section>
+  );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../../components/About/About';
 import Intro from '../../components/Intro/Intro';
+import Stacks from '../../components/Stacks/Stacks';
 
 export default function Home() {
   return (
     <div>
       <Intro />
       <About />
+      <Stacks />
     </div>
   );
 }
