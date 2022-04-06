2318e492c55d9dde2495231b8464d5796a23f36b
ajoy267 <andrew.joy12@hotmail.com>
2022-04-06 15:41:45 -0700

    updated my intro file

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Intro() {
   return (
-    <div className="text-slate-300 my-4 border-t-4 border-slate-300">
+    <section className="text-slate-300 my-4 border-t-4 border-slate-300">
       <h1 className="flex justify-center">Hi! I'm Andrew Joy</h1>
       <p className="flex justify-center">
         I am a Full-Stack Software Developer from Oregon.
       </p>
-    </div>
+    </section>
   );
 }
