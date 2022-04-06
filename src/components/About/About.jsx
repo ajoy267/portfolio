e10a12a5f6a94b5370b35adb9a640eb4eb5b0a69
ajoy267 <andrew.joy12@hotmail.com>
2022-04-06 14:41:55 -0700

    added picture on about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import headshot from '../../assets/headshot.png';
 
 export default function About() {
   return (
     <div>
       <h3>About Me</h3>
       <p>Info</p>
+      <img src={headshot} alt="Profile Picture" />
     </div>
   );
 }
