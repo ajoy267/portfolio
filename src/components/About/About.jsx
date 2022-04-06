1cb8ff07bc8acd3ac142bb4130d09ec22c22aeca
ajoy267 <andrew.joy12@hotmail.com>
2022-04-06 14:05:59 -0700

    made about section and aded oprofile picture

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h3>About Me</h3>
+      <p>Info</p>
+    </div>
+  );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../../components/About/About';
 import Intro from '../../components/Intro/Intro';
 
 export default function Home() {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 }
