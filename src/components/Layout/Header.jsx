d63879f4769d9890f6972a15458d13cd919a816f
ajoy267 <andrew.joy12@hotmail.com>
2022-04-06 11:10:05 -0700

    added header content

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="flex space-x-28 justify-center text-slate-300 my-4">
+      <h2>Home</h2>
+      <h2>About</h2>
+      <h2>Projects</h2>
+      <h2>Tech Stack</h2>
+      <h2>Connect</h2>
+    </header>
+  );
 }
