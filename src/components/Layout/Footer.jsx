7b38312f309d594cd4afa47ab70c1b39740f57eb
ajoy267 <andrew.joy12@hotmail.com>
2022-05-09 17:12:56 -0700

    added the footer content with links

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import github from '../../assets/github.png';
+import linkedin from '../../assets/linkedin.png';
 
 export default function Footer() {
   return (
     <footer>
       <h3>Contact</h3>
+      <div>
+        <a href="https://github.com/ajoy267" target="_blank">
+          <img src={github} alt="github logo" className="h-24 w-24" />
+        </a>
+        <a href="https://www.linkedin.com/in/andrewjoy12/" target="_blank">
+          <img src={linkedin} alt="linkedin logo" className="h-24 w-24" />
+        </a>
+      </div>
     </footer>
   );
 }
