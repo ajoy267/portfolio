a6f86be0462fb36bd779b05ef8c33eced8d6b5ce
ajoy267 <andrew.joy12@hotmail.com>
2022-05-09 17:16:57 -0700

    changed the header in the footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,7 +5,7 @@ import linkedin from '../../assets/linkedin.png';
 export default function Footer() {
   return (
     <footer className="text-slate-300 my-4 border-t-4 border-slate-300">
-      <h3>Contact</h3>
+      <h3>Contact Me</h3>
       <div>
         <a href="https://github.com/ajoy267" target="_blank">
           <img src={github} alt="github logo" className="h-24 w-24" />
